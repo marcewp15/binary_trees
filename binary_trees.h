552c0907471ff960515df164aaee2aa444cfa60e
marcewp15 <970@holbertonschool.com>
2020-03-11 21:40:00 +0000

    [Add] header file with the prototypes of the binary tree project

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,8 @@
 #ifndef BINARY_TREES
 #define BINARY_TREES
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -8,13 +11,20 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-};
+} binary_tree_t;
 
-typedef struct binary_tree_s binary_tree_t;
+void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
+int binary_tree_is_leaf(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node);
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 #endif
