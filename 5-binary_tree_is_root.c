4cc383ce941ad7f09c38af4aac071289fba86a6d
marcewp15 <970@holbertonschool.com>
2020-03-11 22:03:15 +0000

    [Fix] change the documentation, a function that goes through a binary tree using pre-order traversal

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -3,7 +3,7 @@
  * binary_tree_is_root - checks if a given node is a root
  * @node: is a pointer to the node to check
  *
- * Return: 1 if node is a root, or 0 if node is NULL
+ * Return: if tree or func is NULL, do nothing
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
