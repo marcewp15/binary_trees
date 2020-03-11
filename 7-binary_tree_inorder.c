644ad66b14e44512653b4dc9afc482e493b6df84
marcewp15 <970@holbertonschool.com>
2020-03-11 22:23:16 +0000

    [Add] a function that goes through a binary tree using in-order traversal

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_inorder - goes through a binary tree using pre-order traversal
+ * binary_tree_inorder - goes through a binary tree using in-order traversal
  * @tree: is a pointer to the root node of the tree to traverse
  * @func: is a pointer to a function to call for each node
  *
