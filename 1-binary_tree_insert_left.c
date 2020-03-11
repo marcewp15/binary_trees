e471c466e0a3a70481da4733d2af450bcd539b75
marcewp15 <970@holbertonschool.com>
2020-03-11 21:49:54 +0000

    [Fix] condition to evaluate the parent in the binary tree, a function that inserts a node as the left-child of another node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,11 +12,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *new_node;
 	int aux;
 
-	if (parent != NULL)
-	{
-		new_node = binary_tree_node(parent, value);
-	}
+	if (parent == NULL)
 	return (NULL);
+
+	new_node = binary_tree_node(parent, value);
 
 	if (new_node != NULL)
 	{
